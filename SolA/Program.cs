cd7d3ef16d664f471fbdc6551069da4af02e5fc5
yunghans <yunghans@hotmail.com>
2017-08-07 16:31:41 +0800

    Add project files.

---
+++ SolA/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
